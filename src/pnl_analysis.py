912f6d9ec984e19743ce5b97cd54ff5a8f788340
Huang Runping <pingrunhuang@gmail.com>
2023-10-19 14:38:31 +0800

    test pnl calculation

--- src/pnl_analysis.py
+++ src/pnl_analysis.py
@@ -45,49 +45,60 @@ class EMTradesPriorityQueue:
 
     def dequeue(self)->Optional[Trade]:
         try:
-            yield heappop(self.buy_trades)[-1]
+            return heappop(self.buy_trades)[-1]
         except IndexError:
             return None
         
     def enqueue(self, trade:Trade):
         heappush(self.buy_trades, (trade.dt, trade))
     
-    def load_trade(self, symbol:str):
+    def assemble_trade(self, doc:dict):
+        return Trade(doc["symbol"], doc["trade_amt"], doc["trade_prx"], doc["datetime"], doc["trade_fee"], doc["stamp_tax"], doc["transmit_fee"], doc["regulate_fee"], doc["buysell"])
+    
+    def load_trade(self, filter:dict):
         clc = self.mongo_manager.db.get_collection("em_trades")
-        res = clc.find({"symbol":symbol}, sort=[("datetime",1)])
+        res = clc.find(filter, sort=[("datetime",1)])
         if not res:
-            raise ValueError(f'symbol={symbol} not found')
+            raise ValueError(f'documents with filter={filter} not found')
         trades = 0
         for doc in res:
-            yield Trade(doc["symbol"], doc["trade_prx"], doc["trade_amt"], doc["datetime"], doc["trade_fee"], doc["stamp_tax"], doc["transmit_fee"], doc["regulate_fee"], doc["buysell"])
+            yield self.assemble_trade(doc)
             trades+=1
         logger.info(f"Fetched {trades} trades")
+    
+    def _calculate_pnl(self, trade:Trade):
+        logger.info(f"Checking trade: {trade}")
+        if trade.side == "B":
+            self.enqueue(trade)
+        else:
+            logger.info(f"Closing position: {trade}")
+            sell_fee = trade.transmit_fee+trade.trade_fee+trade.stamp_tax+trade.regulate_fee
+            sell_prx = trade.prx
+            sell_qty = trade.qty
+            open_dates = []
+            while sell_qty>0:
+                buy_trade = self.dequeue()
+                buy_fee = buy_trade.transmit_fee+buy_trade.trade_fee+buy_trade.stamp_tax+buy_trade.regulate_fee
+                open_dates.append(buy_trade.dt)
+                if sell_qty>buy_trade.qty:
+                    diff_qty = buy_trade.qty
+                else:
+                    diff_qty = sell_qty
+                    if sell_qty<buy_trade.qty:
+                        buy_trade.qty -= sell_qty
+                        self.enqueue(buy_trade)
+                    else:
+                        logger.info(f"position opened at {open_dates}, closed at {trade.dt}")
+                        self.realized_pnl-=sell_fee
+                self.realized_pnl+=diff_qty*(sell_prx-buy_trade.prx)
+                self.realized_pnl-=buy_fee
+                sell_qty-=diff_qty
+            
 
     def calculate_pnl(self, symbol:str):
-        logger.info(f"Start calculating pnl of {symbol.encode('utf-8')}")
-
-        for trade in self.load_trade(symbol):
-            if trade.side == "B":
-                self.enqueue(trade)
-            else:
-                # sell_fee = trade.transmit_fee+trade.trade_fee+trade.stamp_tax+trade.regulate_fee
-                sell_prx = trade.prx
-                sell_qty = trade.qty
-                while sell_qty>0:
-                    buy_trade = self.dequeue()
-                    # buy_fee = buy_trade.transmit_fee+buy_trade.trade_fee+buy_trade.stamp_tax+buy_trade.regulate_fee
-                    if sell_qty>buy_trade.qty:
-                        diff_qty = buy_trade.qty
-                    else:
-                        diff_qty = sell_qty
-                        if sell_qty<buy_trade.qty:
-                            buy_trade.qty -= sell_qty
-                            self.enqueue(buy_trade)
-                        else:
-                            logger.info(f"Liquidate trade at {trade.dt}")
-                    self.realized_pnl+=diff_qty*(sell_prx-buy_trade.prx)
-                    sell_qty-=diff_qty
-
+        logger.info(f"Start calculating pnl of {symbol}")
+        for trade in self.load_trade({"symbol":symbol}):
+            self._calculate_pnl(trade)
         logger.info(f"Realized pnl: {self.realized_pnl}")
 
 
